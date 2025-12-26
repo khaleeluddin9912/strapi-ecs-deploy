08a92a691e296b19336417baa8d6d53013d60ea3
khaleeluddin9912 <khaleeluddin9912@outlook.com>
2025-12-26 06:45:54 +0530

    Update rds.tf

--- Terraform/rds.tf
+++ Terraform/rds.tf
@@ -28,9 +28,9 @@ resource "aws_db_instance" "strapi_db" {
   instance_class      = var.db_instance_class
   allocated_storage   = 20
 
-  db_name             = var.db_name
-  username            = var.db_username
-  password            = random_password.db_password.result
+  db_name  = var.db_name
+  username = var.db_username
+  password = random_password.db_password.result
 
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
 
