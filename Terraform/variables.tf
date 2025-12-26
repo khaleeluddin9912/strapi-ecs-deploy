37ddfff11b3d96ede5b17004c4c8153d2d53c246
khaleeluddin9912 <khaleeluddin9912@outlook.com>
2025-12-26 08:43:27 +0530

    Update rds.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,43 +1,7 @@
-#################################
-# AWS
-#################################
 variable "aws_region" {
   description = "AWS region for all resources"
   type        = string
   default     = "ap-south-1"
-}
-
-#################################
-# ECS / ECR
-#################################
-variable "image_uri" {
-  description = "Full ECR image URI with tag"
-  type        = string
-  default     = "301782007642.dkr.ecr.ap-south-1.amazonaws.com/khaleel-strapi-app:latest"
-}
-
-#################################
-# Application
-#################################
-variable "environment" {
-  description = "Deployment environment"
-  type        = string
-  default     = "production"
-}
-
-variable "strapi_port" {
-  description = "Port on which Strapi runs inside the container"
-  type        = number
-  default     = 1337
-}
-
-#################################
-# Database
-#################################
-variable "db_engine" {
-  description = "Database engine"
-  type        = string
-  default     = "postgres"
 }
 
 variable "db_name" {
@@ -52,14 +16,32 @@ variable "db_username" {
   default     = "strapiadmin"
 }
 
+variable "db_password" {
+  description = "Strapi database password"
+  type        = string
+  default     = ""  
+}
+
 variable "db_instance_class" {
   description = "RDS instance class"
   type        = string
   default     = "db.t3.micro"
 }
 
-variable "db_password" {
-  description = "Strapi DB password"
+variable "environment" {
+  description = "Deployment environment"
   type        = string
-  default     = "" 
+  default     = "production"
+}
+
+variable "strapi_port" {
+  description = "Port for Strapi container"
+  type        = number
+  default     = 1337
+}
+
+variable "image_uri" {
+  description = "Full ECR image URI with tag"
+  type        = string
+  default     = "301782007642.dkr.ecr.ap-south-1.amazonaws.com/khaleel-strapi-app:latest"
 }
